224a285d83a625f4785be1ca9e93ffecf64e14b5
Tamizh <tamizh@pop-os.localdomain>
2020-11-07 12:52:23 +0530

    fixed typo

--- Server/transvtime.py
+++ Server/transvtime.py
@@ -178,7 +178,7 @@ def typ(s):
 def loan():
     D={}
     idx=[1001,1002,1003,1004,1005,1006,1007,1008,1009,1010]
-    name=["Rashi Bank","Premium Bank Corp.","New Walth Trust","IDIDI Bank","Premium Bank Corp","IDIDI Bank","New Wealth Trust","Rashi Bank","Rashi Bank","New Wealth Trust"]
+    name=["Rashi Bank","Premium Bank Corp.","New Wealth Trust","IDIDI Bank","Premium Bank Corp","IDIDI Bank","New Wealth Trust","Rashi Bank","Rashi Bank","New Wealth Trust"]
     amount=[50000,30000,5000,45000,80000,50000,5000,7000,12500,50000]
     duration=[6,4,1,1,5,8,3,2,3,12]
     ty=["Business","Personal","Business","Personal","Personal","Business","Personal","Business","Personal","Personal"]
