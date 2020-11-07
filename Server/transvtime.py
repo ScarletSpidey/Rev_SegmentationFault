c6e185353403ea46c435fc0f78947145ce6c9814
Tamizh <tamizh@pop-os.localdomain>
2020-11-07 13:46:34 +0530

    editted

--- Server/transvtime.py
+++ Server/transvtime.py
@@ -196,14 +196,49 @@ def loan():
     D=d   
     return D
 def bank():
-    return loan()[0]
-def rashi():
-    Z=loan()
+    def rashi():
     D={}
+    idx=[1001,1002,1003,1004,1005,1006,1007,1008,1009,1010]
+    name=["Rashi Bank","Premium Bank Corp.","New Walth Trust","IDIDI Bank","Premium Bank Corp","IDIDI Bank","New Wealth Trust","Rashi Bank","Rashi Bank","New Wealth Trust"]
+    amount=[50000,30000,5000,45000,80000,50000,5000,7000,12500,50000]
+    duration=[6,4,1,1,5,8,3,2,3,12]
+    ty=["Business","Personal","Business","Personal","Personal","Business","Personal","Business","Personal","Personal"]
+    interest=[10,9,8,10,11,10,10,10.5,10,9.5]
+    for i in range(10):
+        d={}
+        d["id"]=idx[i]
+        d["name"]=name[i]
+        d["amount"]=amount[i]
+        d["duration"]=duration[i]
+        d["type"]=ty[i]
+        d["interest"]=interest[i]
+        D[i]=d
+    
+    for i in D:
+        return D[i]
+def rashi():
+    D={}
+    idx=[1001,1002,1003,1004,1005,1006,1007,1008,1009,1010]
+    name=["Rashi Bank","Premium Bank Corp.","New Walth Trust","IDIDI Bank","Premium Bank Corp","IDIDI Bank","New Wealth Trust","Rashi Bank","Rashi Bank","New Wealth Trust"]
+    amount=[50000,30000,5000,45000,80000,50000,5000,7000,12500,50000]
+    duration=[6,4,1,1,5,8,3,2,3,12]
+    ty=["Business","Personal","Business","Personal","Personal","Business","Personal","Business","Personal","Personal"]
+    interest=[10,9,8,10,11,10,10,10.5,10,9.5]
+    for i in range(10):
+        d={}
+        d["id"]=idx[i]
+        d["name"]=name[i]
+        d["amount"]=amount[i]
+        d["duration"]=duration[i]
+        d["type"]=ty[i]
+        d["interest"]=interest[i]
+        D[i]=d
+    
+    Z={}
     j=0
-    for i in Z:
-        t=Z[i]["name"]
+    for i in D:
+        t=D[i]["name"]
         if t=="Rashi Bank":
-            D[str(j)]=Z[i]
+            Z[str(j)]=D[i]
             j+=1
-    return D
+    return Z
